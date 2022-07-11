66d31b0fc7dcae9ad448a1e4684b5bb6517c111d
Raminka <raminka13@hotmail.com>
2022-07-11 15:46:39 -0500

    Add all files from App

---
+++ index.js
@@ -0,0 +1,46 @@
+import Task from './modules/TaskClass.js';
+import UI from './modules/UI.js';
+import Storage from './modules/Storage.js';
+
+document.addEventListener('DOMContentLoaded', UI.displayTask);
+
+document.querySelector('#form-section').addEventListener('submit', (e) => {
+  // Prevent submit
+  e.preventDefault();
+
+  const description = document.getElementById('add-input').value;
+  const completed = false;
+  const taskId = Storage.getTask().length + 1;
+
+  // Validation
+  if (description === '') {
+    UI.showAlert('Please fill in all fields', 'danger');
+  } else {
+    // Start a new Task
+    const task = new Task(description, completed, taskId);
+
+    // Add Task to UI
+    UI.addTasktoList(task);
+    UI.showAlert('Task Added', 'success');
+
+    // Add Task to LocalStorage
+    Storage.addTask(task);
+
+    // Clear fields
+    UI.clearFields();
+  }
+});
+
+const clearAll = document.getElementById('clear-all');
+const resetBtn = document.getElementById('reset-btn');
+clearAll.addEventListener('click', () => {
+  Storage.clearComplete();
+  UI.showAlert('Tasks Deleted', 'danger');
+  UI.deleteTask();
+});
+
+resetBtn.addEventListener('click', () => {
+  Storage.emptyArr();
+  UI.showAlert('All Tasks Deleted', 'danger');
+  UI.deleteTask();
+});
